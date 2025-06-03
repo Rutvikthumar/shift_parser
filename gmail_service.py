21662a3852fe3fc838afac8824b33e64b506bf0e
Rutvikthumar <rutvikthumar@gmail.com>
2025-06-03 10:50:59 -0400

    Update gmail_service.py

--- gmail_service.py
+++ gmail_service.py
@@ -1,30 +1,11 @@
-import os
 import base64
 from googleapiclient.discovery import build
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
 
-# If modifying these SCOPES, delete the token.json.
-SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
-
-def get_gmail_service():
-    creds = None
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+def get_gmail_service(creds):
     return build('gmail', 'v1', credentials=creds)
 
-def get_shift_emails():
-    service = get_gmail_service()
-    # Search for emails with subject
+def get_shift_emails(creds):
+    service = get_gmail_service(creds)
     results = service.users().messages().list(
         userId='me',
         q='"TAL Pharmacy Recruitment Inc. Shifts"'
@@ -41,6 +22,5 @@ def get_shift_emails():
                     output.append({'id': msg['id'], 'body': body})
                     break
         else:
-            # fallback: try snippet
             output.append({'id': msg['id'], 'body': msg_detail.get('snippet', '')})
     return output
