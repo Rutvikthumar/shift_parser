b674faf3c8f5e7bbfff4611770feaa5ede85abc9
Rutvikthumar <rutvikthumar@gmail.com>
2025-06-03 10:31:04 -0400

    Delete drive_parser.py

--- drive_parser.py
+++
@@ -1,7 +0,0 @@
-def parse_drive_shifts(processed_ids):
-    """
-    Fetch and parse WhatsApp chat text files from Drive ZIPs, skipping processed_ids.
-    Return: (list of shift dicts, list of new Drive file IDs)
-    """
-    # TODO: Implement Drive API, ZIP extraction, and parsing logic
-    return [], []
