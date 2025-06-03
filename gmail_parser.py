383539141adc0df0b97a7787e00984704603ca96
Rutvikthumar <rutvikthumar@gmail.com>
2025-06-03 10:31:49 -0400

    Delete gmail_parser.py

--- gmail_parser.py
+++
@@ -1,8 +0,0 @@
-def parse_gmail_shifts(processed_ids):
-    """
-    Fetch and parse Gmail shift emails, skipping processed_ids.
-    Return: (list of shift dicts, list of new Gmail message IDs)
-    """
-    # TODO: Implement Gmail API fetching and robust parsing
-    # Use regex and dateutil.parser for flexible date/time handling
-    return [], []
